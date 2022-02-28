e03dfa3c08938c991165ca54083ab8b41142dd1f
suckyoudry <98239901+suckyoudry@users.noreply.github.com>
2022-02-28 00:20:43 +0000

    Add files via upload

--- Stock Market/Stock Market/Form1.Designer.vb
+++ Stock Market/Stock Market/Form1.Designer.vb
@@ -25,23 +25,29 @@ Partial Class Form1
         Dim ChartArea1 As System.Windows.Forms.DataVisualization.Charting.ChartArea = New System.Windows.Forms.DataVisualization.Charting.ChartArea()
         Dim Legend1 As System.Windows.Forms.DataVisualization.Charting.Legend = New System.Windows.Forms.DataVisualization.Charting.Legend()
         Dim Series1 As System.Windows.Forms.DataVisualization.Charting.Series = New System.Windows.Forms.DataVisualization.Charting.Series()
-        Me.Button1 = New System.Windows.Forms.Button()
         Me.Chart1 = New System.Windows.Forms.DataVisualization.Charting.Chart()
         Me.ShareAverage = New System.Windows.Forms.TextBox()
         Me.FTSEAverage = New System.Windows.Forms.TextBox()
-        Me.FTSETrendText = New System.Windows.Forms.TextBox()
+        Me.FTSEPrediction = New System.Windows.Forms.TextBox()
+        Me.ShareBox1 = New System.Windows.Forms.TextBox()
+        Me.ShareBox2 = New System.Windows.Forms.TextBox()
+        Me.ShareBox3 = New System.Windows.Forms.TextBox()
+        Me.ShareBox4 = New System.Windows.Forms.TextBox()
+        Me.ShareBox5 = New System.Windows.Forms.TextBox()
+        Me.FTSEBox1 = New System.Windows.Forms.TextBox()
+        Me.FTSEBox2 = New System.Windows.Forms.TextBox()
+        Me.FTSEBox3 = New System.Windows.Forms.TextBox()
+        Me.FTSEBox4 = New System.Windows.Forms.TextBox()
+        Me.FTSEBox5 = New System.Windows.Forms.TextBox()
+        Me.SharePrediction = New System.Windows.Forms.TextBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.ShareNameText = New System.Windows.Forms.TextBox()
+        Me.HyperlinkText = New System.Windows.Forms.TextBox()
+        Me.SharePercentage = New System.Windows.Forms.TextBox()
+        Me.FTSEPercentage = New System.Windows.Forms.TextBox()
         CType(Me.Chart1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
-        '
-        'Button1
-        '
-        Me.Button1.Location = New System.Drawing.Point(9, 10)
-        Me.Button1.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(128, 49)
-        Me.Button1.TabIndex = 0
-        Me.Button1.Text = "Button1"
-        Me.Button1.UseVisualStyleBackColor = True
         '
         'Chart1
         '
@@ -49,8 +55,8 @@ Partial Class Form1
         Me.Chart1.ChartAreas.Add(ChartArea1)
         Legend1.Name = "Legend1"
         Me.Chart1.Legends.Add(Legend1)
-        Me.Chart1.Location = New System.Drawing.Point(163, 10)
-        Me.Chart1.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.Chart1.Location = New System.Drawing.Point(234, 59)
+        Me.Chart1.Margin = New System.Windows.Forms.Padding(2)
         Me.Chart1.Name = "Chart1"
         Series1.ChartArea = "ChartArea1"
         Series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line
@@ -63,37 +69,176 @@ Partial Class Form1
         '
         'ShareAverage
         '
-        Me.ShareAverage.Location = New System.Drawing.Point(9, 348)
+        Me.ShareAverage.Location = New System.Drawing.Point(77, 462)
         Me.ShareAverage.Name = "ShareAverage"
         Me.ShareAverage.Size = New System.Drawing.Size(62, 20)
         Me.ShareAverage.TabIndex = 2
         '
         'FTSEAverage
         '
-        Me.FTSEAverage.Location = New System.Drawing.Point(77, 348)
+        Me.FTSEAverage.Location = New System.Drawing.Point(145, 462)
         Me.FTSEAverage.Name = "FTSEAverage"
-        Me.FTSEAverage.Size = New System.Drawing.Size(60, 20)
+        Me.FTSEAverage.Size = New System.Drawing.Size(62, 20)
         Me.FTSEAverage.TabIndex = 3
         '
-        'FTSETrendText
+        'FTSEPrediction
         '
-        Me.FTSETrendText.Location = New System.Drawing.Point(579, 444)
-        Me.FTSETrendText.Name = "FTSETrendText"
-        Me.FTSETrendText.Size = New System.Drawing.Size(100, 20)
-        Me.FTSETrendText.TabIndex = 4
+        Me.FTSEPrediction.Location = New System.Drawing.Point(745, 462)
+        Me.FTSEPrediction.Name = "FTSEPrediction"
+        Me.FTSEPrediction.Size = New System.Drawing.Size(100, 20)
+        Me.FTSEPrediction.TabIndex = 4
+        '
+        'ShareBox1
+        '
+        Me.ShareBox1.Location = New System.Drawing.Point(77, 333)
+        Me.ShareBox1.Name = "ShareBox1"
+        Me.ShareBox1.Size = New System.Drawing.Size(62, 20)
+        Me.ShareBox1.TabIndex = 5
+        '
+        'ShareBox2
+        '
+        Me.ShareBox2.Location = New System.Drawing.Point(77, 307)
+        Me.ShareBox2.Name = "ShareBox2"
+        Me.ShareBox2.Size = New System.Drawing.Size(62, 20)
+        Me.ShareBox2.TabIndex = 6
+        '
+        'ShareBox3
+        '
+        Me.ShareBox3.Location = New System.Drawing.Point(77, 281)
+        Me.ShareBox3.Name = "ShareBox3"
+        Me.ShareBox3.Size = New System.Drawing.Size(62, 20)
+        Me.ShareBox3.TabIndex = 7
+        '
+        'ShareBox4
+        '
+        Me.ShareBox4.Location = New System.Drawing.Point(77, 255)
+        Me.ShareBox4.Name = "ShareBox4"
+        Me.ShareBox4.Size = New System.Drawing.Size(62, 20)
+        Me.ShareBox4.TabIndex = 8
+        '
+        'ShareBox5
+        '
+        Me.ShareBox5.Location = New System.Drawing.Point(77, 229)
+        Me.ShareBox5.Name = "ShareBox5"
+        Me.ShareBox5.Size = New System.Drawing.Size(62, 20)
+        Me.ShareBox5.TabIndex = 9
+        '
+        'FTSEBox1
+        '
+        Me.FTSEBox1.Location = New System.Drawing.Point(145, 333)
+        Me.FTSEBox1.Name = "FTSEBox1"
+        Me.FTSEBox1.Size = New System.Drawing.Size(62, 20)
+        Me.FTSEBox1.TabIndex = 10
+        '
+        'FTSEBox2
+        '
+        Me.FTSEBox2.Location = New System.Drawing.Point(145, 307)
+        Me.FTSEBox2.Name = "FTSEBox2"
+        Me.FTSEBox2.Size = New System.Drawing.Size(62, 20)
+        Me.FTSEBox2.TabIndex = 11
+        '
+        'FTSEBox3
+        '
+        Me.FTSEBox3.Location = New System.Drawing.Point(145, 281)
+        Me.FTSEBox3.Name = "FTSEBox3"
+        Me.FTSEBox3.Size = New System.Drawing.Size(62, 20)
+        Me.FTSEBox3.TabIndex = 12
+        '
+        'FTSEBox4
+        '
+        Me.FTSEBox4.Location = New System.Drawing.Point(145, 255)
+        Me.FTSEBox4.Name = "FTSEBox4"
+        Me.FTSEBox4.Size = New System.Drawing.Size(62, 20)
+        Me.FTSEBox4.TabIndex = 13
+        '
+        'FTSEBox5
+        '
+        Me.FTSEBox5.Location = New System.Drawing.Point(145, 229)
+        Me.FTSEBox5.Name = "FTSEBox5"
+        Me.FTSEBox5.Size = New System.Drawing.Size(62, 20)
+        Me.FTSEBox5.TabIndex = 14
+        '
+        'SharePrediction
+        '
+        Me.SharePrediction.Location = New System.Drawing.Point(851, 462)
+        Me.SharePrediction.Name = "SharePrediction"
+        Me.SharePrediction.Size = New System.Drawing.Size(100, 20)
+        Me.SharePrediction.TabIndex = 15
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(992, 10)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 16
+        Me.Button2.Text = "Exit"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.Location = New System.Drawing.Point(77, 13)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(130, 173)
+        Me.ListBox1.TabIndex = 17
+        '
+        'ShareNameText
+        '
+        Me.ShareNameText.Location = New System.Drawing.Point(234, 13)
+        Me.ShareNameText.Name = "ShareNameText"
+        Me.ShareNameText.Size = New System.Drawing.Size(183, 20)
+        Me.ShareNameText.TabIndex = 18
+        '
+        'HyperlinkText
+        '
+        Me.HyperlinkText.Location = New System.Drawing.Point(423, 13)
+        Me.HyperlinkText.Name = "HyperlinkText"
+        Me.HyperlinkText.Size = New System.Drawing.Size(200, 20)
+        Me.HyperlinkText.TabIndex = 19
+        Me.HyperlinkText.Text = " "
+        '
+        'SharePercentage
+        '
+        Me.SharePercentage.Location = New System.Drawing.Point(77, 203)
+        Me.SharePercentage.Name = "SharePercentage"
+        Me.SharePercentage.Size = New System.Drawing.Size(62, 20)
+        Me.SharePercentage.TabIndex = 20
+        '
+        'FTSEPercentage
+        '
+        Me.FTSEPercentage.Location = New System.Drawing.Point(145, 203)
+        Me.FTSEPercentage.Name = "FTSEPercentage"
+        Me.FTSEPercentage.Size = New System.Drawing.Size(62, 20)
+        Me.FTSEPercentage.TabIndex = 21
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.SystemColors.Menu
-        Me.ClientSize = New System.Drawing.Size(978, 549)
-        Me.Controls.Add(Me.FTSETrendText)
+        Me.ClientSize = New System.Drawing.Size(1111, 609)
+        Me.Controls.Add(Me.FTSEPercentage)
+        Me.Controls.Add(Me.SharePercentage)
+        Me.Controls.Add(Me.HyperlinkText)
+        Me.Controls.Add(Me.ShareNameText)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.SharePrediction)
+        Me.Controls.Add(Me.FTSEBox5)
+        Me.Controls.Add(Me.FTSEBox4)
+        Me.Controls.Add(Me.FTSEBox3)
+        Me.Controls.Add(Me.FTSEBox2)
+        Me.Controls.Add(Me.FTSEBox1)
+        Me.Controls.Add(Me.ShareBox5)
+        Me.Controls.Add(Me.ShareBox4)
+        Me.Controls.Add(Me.ShareBox3)
+        Me.Controls.Add(Me.ShareBox2)
+        Me.Controls.Add(Me.ShareBox1)
+        Me.Controls.Add(Me.FTSEPrediction)
         Me.Controls.Add(Me.FTSEAverage)
         Me.Controls.Add(Me.ShareAverage)
         Me.Controls.Add(Me.Chart1)
-        Me.Controls.Add(Me.Button1)
-        Me.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.Margin = New System.Windows.Forms.Padding(2)
         Me.Name = "Form1"
         Me.Text = "Form1"
         CType(Me.Chart1, System.ComponentModel.ISupportInitialize).EndInit()
@@ -101,10 +246,25 @@ Partial Class Form1
         Me.PerformLayout()
 
     End Sub
-
-    Friend WithEvents Button1 As Button
     Friend WithEvents Chart1 As DataVisualization.Charting.Chart
     Friend WithEvents ShareAverage As TextBox
     Friend WithEvents FTSEAverage As TextBox
-    Friend WithEvents FTSETrendText As TextBox
+    Friend WithEvents FTSEPrediction As TextBox
+    Friend WithEvents ShareBox1 As TextBox
+    Friend WithEvents ShareBox2 As TextBox
+    Friend WithEvents ShareBox3 As TextBox
+    Friend WithEvents ShareBox4 As TextBox
+    Friend WithEvents ShareBox5 As TextBox
+    Friend WithEvents FTSEBox1 As TextBox
+    Friend WithEvents FTSEBox2 As TextBox
+    Friend WithEvents FTSEBox3 As TextBox
+    Friend WithEvents FTSEBox4 As TextBox
+    Friend WithEvents FTSEBox5 As TextBox
+    Friend WithEvents SharePrediction As TextBox
+    Friend WithEvents Button2 As Button
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents ShareNameText As TextBox
+    Friend WithEvents HyperlinkText As TextBox
+    Friend WithEvents SharePercentage As TextBox
+    Friend WithEvents FTSEPercentage As TextBox
 End Class
--- Stock Market/Stock Market/Form1.vb
+++ Stock Market/Stock Market/Form1.vb
@@ -1,31 +1,35 @@
 ﻿Imports Excel = Microsoft.Office.Interop.Excel
 Public Class Form1
+    Dim UserSelection As Integer
 
-    Dim IndexDates(4, 100) As String
-    Dim IndexOpen(4, 100) As Decimal
-    Dim IndexHigh(4, 100) As Decimal
-    Dim IndexLow(4, 100) As Decimal
-    Dim IndexClosingPrice(4, 100) As Decimal
-    Dim IndexAdjClose(4, 100) As Decimal
-    Dim IndexVolume(4, 100) As Decimal
+    Dim IndexDates(4, 21) As String
+    Dim IndexOpen(4, 21) As Decimal
+    Dim IndexHigh(4, 21) As Decimal
+    Dim IndexLow(4, 21) As Decimal
+    Dim IndexClosingPrice(4, 21) As Decimal
+    Dim IndexAdjClose(4, 21) As Decimal
+    Dim IndexVolume(4, 21) As Decimal
 
-    Dim sharename(10) As String
+    Dim sharename(10, 10) As String
     Dim Sharedates(10, 100) As String
     Dim ClosingPrice(10, 100) As Decimal
     Dim Highprice(10, 100) As Decimal
     Dim Lowprice(10, 100) As Decimal
     Dim Openprice(10, 100) As Decimal
 
-    Dim FiveDayShare(10, 5) As Decimal
     Dim ShareAVG(10) As Decimal
+    Dim FiveDayShareTrend(10, 5) As Decimal
 
-    Dim FiveDayFTSE(1, 5) As Decimal
+    Dim FiveDayFTSETrend(1, 5) As Decimal
     Dim FTSEAVG(1) As Decimal
 
     Dim FTSETrend As Decimal
     Dim DJTrend As Decimal
     Dim HSTrend As Decimal
     Dim NTrend As Decimal
+
+    Dim Prediction As Decimal
+    Dim PredictedShare As Decimal
 
     Dim xpos As Integer
     Dim temparea As String
@@ -40,7 +44,7 @@ Public Class Form1
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles Me.Load
 
         'Open Worksheet for each index then read into data arrays each excel cell required for each index
-        'selecting the correct file & worksheet
+        'selecting the correct file & worksheet.  The top (header) line is ignored hence for i=2 to 100
         xlApp = New Excel.Application
         xlWorkBook = xlApp.Workbooks.Open("Z:\Fullbrook A-Levels\Computer Science\computing project\Index Closing Prices.xlsx")
 
@@ -48,7 +52,7 @@ Public Class Form1
             xlWorkSheet = xlWorkBook.Worksheets("Index" + CStr(n))
 
             If n < 2 Then ' this is making the first index which is formatted differently work in the same loop
-                For i = 2 To 100
+                For i = 2 To 21
                     IndexDates(n, i - 1) = (xlWorkSheet.Cells(i, 1).value)
                     temparea = (xlWorkSheet.Cells(i, 3).value) 'i had to use a temp area because the code didn't work trying to search for "-" as a block of code
                     If temparea = "-" Then
@@ -77,7 +81,7 @@ Public Class Form1
                 Next
 
             Else
-                For i = 2 To 100
+                For i = 2 To 21
                     IndexDates(n, i - 1) = (xlWorkSheet.Cells(i, 1).value)
                     temparea = (xlWorkSheet.Cells(i, 2).value)
                     If temparea = "-" Then
@@ -107,14 +111,19 @@ Public Class Form1
             End If
         Next
 
+        xlWorkBook.Close()
+        xlApp.Quit()
+
         'Open worksheet for each share and then read into data arrays each excel cell required for each share
+        'The top (header) line is ignored hence for i=2 to 100
 
         xlApp = New Excel.Application
-        xlWorkBook = xlApp.Workbooks.Open("Z:\Fullbrook A-Levels\Computer Science\graphing\database for markets.xlsx")
+        xlWorkBook = xlApp.Workbooks.Open("Z:\Fullbrook A-Levels\Computer Science\computing project\database for markets.xlsx")
 
         For n = 1 To 10
             xlWorkSheet = xlWorkBook.Worksheets("Share" + CStr(n))
             For i = 2 To 101
+
                 Sharedates(n, i - 1) = (xlWorkSheet.Cells(i, 1).value)
                 temparea = (xlWorkSheet.Cells(i, 2).value)
                 If temparea = "-" Then
@@ -140,33 +149,96 @@ Public Class Form1
                 Else
                     ClosingPrice(n, i - 1) = Val(temparea)
                 End If
+
             Next
         Next
 
+        'gives a list of the share names
         For i = 1 To 10
-            xlWorkSheet = xlWorkBook.Worksheets("Sharename")
-            sharename(i) = (xlWorkSheet.Cells(i, 1).value)
+            For n = 1 To 2 'the second column is going to get the hyperlink for the share names
+                xlWorkSheet = xlWorkBook.Worksheets("Sharename")
+                sharename(i, n) = (xlWorkSheet.Cells(i, n).value)
+            Next
         Next
-
 
         xlWorkBook.Close()
         xlApp.Quit()
 
-        FiveDayShareAVG()
-        FiveDayFTSEAVG()
+        ' loads the options for the listbox
 
+        For i = 1 To 10
+            ListBox1.Items.Add(sharename(i, 1))
+        Next
+
+
+
+        FiveDayFTSEAVG()
+        FiveDayShareAVG()
 
         FTSEOvernightTrend()
         DowJonesOvernightTrend()
+        HangSengOvernightTrend()
+        NikkeiOvernightTrend()
+
+        FTSEPredictor()
+
+
+
+
+
+
+    End Sub
+    Private Sub ListBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ListBox1.SelectedIndexChanged
+        'Add 1 to the list box selection given this starts at zero
+        UserSelection = ListBox1.SelectedIndex + 1
+
+        SharePredictor()
+
+        xpos = 1
+
+        For i = 99 To 1 Step -1
+            Me.Chart1.Series("Closing Price").Points.AddXY(xpos, ClosingPrice(UserSelection, i))
+            xpos = xpos + 1
+        Next
+
+        Me.ShareNameText.Text = (sharename(UserSelection, 1))
+        Me.HyperlinkText.Text = (sharename(UserSelection, 2))
+
+        Me.SharePercentage.Text = "Share %"
+        Me.FTSEPercentage.Text = "FTSE %"
+
+        Me.ShareBox1.Text = (FiveDayShareTrend(UserSelection, 5))
+        Me.ShareBox2.Text = (FiveDayShareTrend(UserSelection, 4))
+        Me.ShareBox3.Text = (FiveDayShareTrend(UserSelection, 3))
+        Me.ShareBox4.Text = (FiveDayShareTrend(UserSelection, 2))
+        Me.ShareBox5.Text = (FiveDayShareTrend(UserSelection, 1))
+
+        Me.FTSEBox1.Text = (FiveDayFTSETrend(1, 5))
+        Me.FTSEBox2.Text = (FiveDayFTSETrend(1, 4))
+        Me.FTSEBox3.Text = (FiveDayFTSETrend(1, 3))
+        Me.FTSEBox4.Text = (FiveDayFTSETrend(1, 2))
+        Me.FTSEBox5.Text = (FiveDayFTSETrend(1, 1))
+
+        Me.ShareAverage.Text = (ShareAVG(UserSelection))
+        Me.FTSEAverage.Text = (FTSEAVG(1))
+
+        Me.FTSEPrediction.Text = (Prediction)
+        Me.SharePrediction.Text = (PredictedShare)
+
+
+
+
     End Sub
 
     Function FiveDayShareAVG() As Decimal
-        ' this functions is going to get the 5 day average of the chosen share
+
+        ' this functions is going to get the 5 day average percentage change of the chosen share
         For i = 1 To 10
             For n = 1 To 5
 
-                FiveDayShare(i, n) = ClosingPrice(i, n)
-                ShareAVG(i) += FiveDayShare(i, n)
+                FiveDayShareTrend(i, n) = ((ClosingPrice(i, n) - ClosingPrice(i, n + 1)) / ClosingPrice(i, n + 1)) * 100
+
+                ShareAVG(i) += FiveDayShareTrend(i, n)
 
             Next
 
@@ -177,61 +249,72 @@ Public Class Form1
     End Function
 
     Function FiveDayFTSEAVG() As Decimal
-        ' going to give the average of the FTSE index over the course of 5 days
-        For i = 1 To 1
-            For n = 1 To 5
+        ' going to give the average percentage change of the FTSE index over the course of 5 days
 
-                FiveDayFTSE(i, n) = IndexClosingPrice(i, n)
-                FTSEAVG(i) += FiveDayFTSE(i, n)
+        For n = 1 To 5
 
-            Next
+            FiveDayFTSETrend(1, n) = ((IndexClosingPrice(1, n) - IndexClosingPrice(1, n + 1)) / IndexClosingPrice(1, n + 1)) * 100
 
-            FTSEAVG(i) = FTSEAVG(i) / 5
+            FTSEAVG(1) += FiveDayFTSETrend(1, n)
+
         Next
+
+        FTSEAVG(1) = FTSEAVG(1) / 5
+
 
         Return FiveDayFTSEAVG
     End Function
 
     Function FTSEOvernightTrend() As Decimal
-
-        FTSETrend = ((IndexClosingPrice(1, 2) - IndexClosingPrice(1, 2 + 1)) / IndexClosingPrice(1, 2 + 1)) * 100
+        'calculates the overnight trend for the FTSE index
+        FTSETrend = ((IndexClosingPrice(1, 1) - IndexClosingPrice(1, 2)) / IndexClosingPrice(1, 2)) * 100
 
         Return FTSEOvernightTrend
     End Function
 
     Function DowJonesOvernightTrend() As Decimal
-
-        DJTrend = ((IndexClosingPrice(2, 2) - IndexClosingPrice(2, 2 + 1)) / IndexClosingPrice(2, 2 + 1))
+        'calculates the overnight trend for the Dow Jones index
+        DJTrend = ((IndexClosingPrice(2, 1) - IndexClosingPrice(2, 2)) / IndexClosingPrice(2, 2)) * 100
 
         Return DowJonesOvernightTrend
     End Function
 
     Function HangSengOvernightTrend() As Decimal
-
-        HSTrend = ((IndexClosingPrice(3, 1) - IndexClosingPrice(3, 1 + 1)) / IndexClosingPrice(3, 1 + 1))
+        'calculates the overnight trend for the Hang Seng index
+        HSTrend = ((IndexClosingPrice(3, 1) - IndexClosingPrice(3, 2)) / IndexClosingPrice(3, 2)) * 100
 
         Return HangSengOvernightTrend
     End Function
 
     Function NikkeiOvernightTrend() As Decimal
-
-        HSTrend = ((IndexClosingPrice(4, 1) - IndexClosingPrice(4, 1 + 1)) / IndexClosingPrice(4, 1 + 1))
+        'calculates the overnight trend for the Nikkei index
+        NTrend = ((IndexClosingPrice(4, 1) - IndexClosingPrice(4, 2)) / IndexClosingPrice(4, 2)) * 100
 
         Return NikkeiOvernightTrend
     End Function
 
-    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+    Function FTSEPredictor() As Decimal
+        'calculates a prediction for the FTSE index
+        Prediction = (FTSETrend + DJTrend + NTrend + HSTrend) / 4
 
-        xpos = 1
+        Return FTSEPredictor
+    End Function
 
-        For i = 99 To 1 Step -1
-            Me.Chart1.Series("Closing Price").Points.AddXY(xpos, ClosingPrice(1, i))
-            xpos = xpos + 1
-        Next
+    Function SharePredictor() As Decimal
 
-        Me.ShareAverage.Text = (ShareAVG(1))
-        Me.FTSEAverage.Text = (FTSEAVG(1))
 
-        Me.FTSETrendText.Text = (FTSETrend)
+        'calculates a prediction for the chosen share
+        PredictedShare = (ShareAVG(UserSelection) / FTSEAVG(1)) * Prediction
+
+        Return SharePredictor
+    End Function
+
+
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+
+        End
+
     End Sub
+
 End Class
